b49572c5d597b027167dce2ae40682ea37faa586
Noah Perret <noah.perret.pro@gmail.com>
2026-08-06 10:00:59 +0200

    refactor LRN bench: extract common + fix openmp for matrix mult

--- gpu4s_benchmark/LRN_bench/openmp/omp_common.cpp
+++ gpu4s_benchmark/LRN_bench/openmp/omp_common.cpp
@@ -1,47 +1,46 @@
 /** * ====================================================================
- * @file        openmp_common.cpp (./matrix_multiplication_bench)
+ * @file        omp_common.cpp (./LRN_bench)
  * @brief       Common OpenMP platform initialization, device setup, 
  *              profiling timer evaluation, and generic cleanup routines.
  * @paragraph   License
  * ESA-PL Strong Copyleft – v2.5
  * ======================================================================= */
 #include "../benchmark_library.h"
-
 #include <cstring>
 
 
-void init(GraficCommon* device_object, char* device_name){
+void init(GraficCommon* device_object, char* device_name)
+{
 	init(device_object, 0,0, device_name);
 }
 
 
-void init(GraficCommon* device_object, int platform, int device, char* device_name)
+void init(GraficCommon* device_object, int platform ,int device, char* device_name)
 {
 	// TBD Feature: device name. -- Bulky generic platform implementation
 	strcpy(device_name,"Generic device");
 }
 
 
-bool device_memory_init(GraficCommon* device_object, unsigned int size_a_matrix, unsigned int size_b_matrix, unsigned int size_c_matrix) 
+bool device_memory_init(GraficCommon* device_object, unsigned int size_a_matrix, unsigned int size_b_matrix)
 {
 	GraficObject* deviceObj = static_cast<GraficObject*>(device_object);
-	deviceObj->d_C = (bench_t*) malloc ( size_c_matrix * sizeof(bench_t*));
+	deviceObj->d_B = (bench_t*) malloc ( size_b_matrix * sizeof(bench_t*));
    	return true;
 }
 
 
-void copy_memory_to_device(GraficCommon* device_object, bench_t* h_A, bench_t* h_B, unsigned int size_a, unsigned int size_b)
+void copy_memory_to_device(GraficCommon* device_object, bench_t* h_A, unsigned int size_a)
 {
 	GraficObject* deviceObj = static_cast<GraficObject*>(device_object);
 	deviceObj->d_A = h_A;
-	deviceObj->d_B = h_B;
 }
 
 
 void copy_memory_to_host(GraficCommon* device_object, bench_t* h_C, int size)
 {
 	GraficObject* deviceObj = static_cast<GraficObject*>(device_object);	     
-	memcpy(h_C, &deviceObj->d_C[0], sizeof(bench_t)*size);
+	memcpy(h_C, &deviceObj->d_B[0], sizeof(bench_t)*size);
 }
 
 
@@ -49,9 +48,10 @@ float get_elapsed_time(GraficCommon* device_object, bool csv_format, bool csv_fo
 {
 	GraficObject* deviceObj = static_cast<GraficObject*>(device_object);
 	if (csv_format_timestamp){
-        printf("%.10f;%.10f;%.10f;%ld;\n",(bench_t) 0, deviceObj->elapsed_time * 1000.f, (bench_t) 0, current_time);
+        printf("%.10f;%.10f;%.10f;%ld;\n", (bench_t) 0, deviceObj->elapsed_time * 1000.f, (bench_t) 0, current_time);
     }
-    else if (csv_format){
+    else if (csv_format)
+	{
         printf("%.10f;%.10f;%.10f;\n", (bench_t) 0, deviceObj->elapsed_time * 1000.f, (bench_t) 0);
     } 
 	else
@@ -60,12 +60,12 @@ float get_elapsed_time(GraficCommon* device_object, bool csv_format, bool csv_fo
 		printf("Elapsed time kernel: %.10f milliseconds\n", deviceObj->elapsed_time * 1000.f);
 		printf("Elapsed time Device->Host: %.10f milliseconds\n", (bench_t) 0);
     }
-    return deviceObj->elapsed_time * 1000.f;
+	return deviceObj->elapsed_time * 1000.f;
 }
 
 
 void clean(GraficCommon* device_object)
 {
 	GraficObject* deviceObj = static_cast<GraficObject*>(device_object);
-	free(deviceObj->d_C);
+	free(deviceObj->d_B);
 }
--- gpu4s_benchmark/LRN_bench/openmp/omp_common.h
+++ gpu4s_benchmark/LRN_bench/openmp/omp_common.h
@@ -1,5 +1,5 @@
 /** * ====================================================================
- * @file        openmp_common.h (./matrix_multiplication_bench)
+ * @file        omp_common.h (./LRN_bench)
  * @brief       Shared declarations, data structures, and timing utilities
  *              for OpenMP benchmark backends.
  * @paragraph   License
